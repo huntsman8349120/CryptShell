67c387293e21a79abd8b8148a25b89d99a0b51aa
huntsman8349120 <russoghzlinfez@gmail.com>
2026-01-08 18:38:26 -0300

    Add main print statements to print.lua

---
+++ print.lua
@@ -0,0 +1,10 @@
+-- |===================== MAIN PRINT'S =====================|
+
+print("|===================== Config =====================|")
+print("Loading script                                      ")
+print("Loading package                                     ")
+print("Loading library                                     ")
+print("Loading Variables                                   ")
+print("Successfully, enjoy                                 ")
+print("Version 1.0, thanks for using my script :D          ")
+print("|==================================================|")
