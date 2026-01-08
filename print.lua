ced84620459873dc249a31f0ee6f28db5c178e9a
huntsman8349120 <russoghzlinfez@gmail.com>
2026-01-08 18:47:47 -0300

    Refactor print statements for better formatting

--- print.lua
+++ print.lua
@@ -1,10 +1,8 @@
--- |===================== MAIN PRINT'S =====================|
-
-print("|===================== Config =====================|")
-print("Loading script                                      ")
-print("Loading package                                     ")
-print("Loading library                                     ")
-print("Loading Variables                                   ")
-print("Successfully, enjoy                                 ")
-print("Version 1.0, thanks for using my script :D          ")
+print("|====================[ Config ]====================|")
+print("| Loading script                                   |")
+print("| Loading package                                  |")
+print("| Loading library                                  |")
+print("| Loading Variables                                |")
+print("| Successfully, enjoy                              |")
+print("| Version 1.0, thanks for using my script :D       |")
 print("|==================================================|")
